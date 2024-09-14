75a3e28accbaefe6835199ff93ac74c791e65005
GorodGood <148943341+GorodGood@users.noreply.github.com>
2024-09-14 18:27:01 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,13 +1,14 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-ind = 0
-while ind < len(my_list):
-    if my_list[ind] > 0:
-        print(my_list[ind])
-        ind += 1
+inx = 0
+while inx < len(my_list):
+    if my_list[inx] > 0:
+        print(my_list[inx])
+        inx += 1
         continue
-    elif my_list[ind] == 0:
-        ind += 1
+    elif my_list[inx] == 0:
+        inx += 1
     else:
-        print('Отрицательное число')
-    break
+        print()
+        break
+
 
